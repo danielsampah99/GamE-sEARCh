0c3853976b9bda3a2ea02bc3b2decee7794ec602
danielsampah99 <danielsampah99@gmail.com>
2023-11-28 23:05:22 +0000

    UI Enhancement: Added loading skeletons to the main view.

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,13 +1,20 @@
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
-	const { games, error } = useGames();
+	const { games, error, isLoading } = useGames();
+
+	const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 	return (
 		<>
 			{error && <p>{error}</p>}
 			<div className="grid grid-cols-1 justify-around gap-y-8 px-4 md:col-span-4 md:grid-cols-2 md:gap-x-7 lg:col-span-4 lg:grid-cols-3">
+				{skeletons.map(
+					(skeleton) =>
+						isLoading && <GameCardSkeleton key={skeleton} />,
+				)}
 				{games.map((game) => (
 					<GameCard key={game.id} game={game} />
 				))}
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -12,7 +12,7 @@ export interface Game {
 	id: number;
 	name: string;
 	background_image: string;
-	parent_platforms: { platform: Platform}[];
+	parent_platforms: { platform: Platform }[];
 	metacritic: number;
 }
 
@@ -21,26 +21,30 @@ interface FetchedGamesSchema {
 	results: Game[];
 }
 const useGames = () => {
-
 	const [games, setGames] = useState<Game[]>([]);
 	const [error, setError] = useState("");
+	const [isLoading, setIsLoading] = useState(false);
 
 	useEffect(() => {
 		const controller = new AbortController();
 
+		setIsLoading(true);
 		apiClient
 			.get<FetchedGamesSchema>("/games", { signal: controller.signal })
-			.then((res) => setGames(res.data.results))
+			.then((res) => {
+				setGames(res.data.results);
+				setIsLoading(false);
+			})
 			.catch((err) => {
 				if (err instanceof CanceledError) return;
 				setError(err.message);
+				setIsLoading(false);
 			});
 
 		return () => controller.abort();
 	}, []);
 
-	return { games, error };
+	return { games, error, isLoading };
 };
 
 export default useGames;
-
